0cf203c07ccea3b5956dc5bcb59a183d066f576d
Your Name <smithbarbose57@gmail.com>
2021-05-05 23:31:35 +0530

    LogisticRegression

---
+++ Logistic Regression.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  6 20:35:56 2021
+
+Logistic Regression using pytorch 
+
+@author: smith_barbose @neb.tics
+"""
+'''
+Step 1) Design model (input,output_size,forward_pass)
+Step 2) Construct loss and optimizer 
+Step 3) Traning Loop:
+    -Forward pass: compute prediction & loss
+    -Backward pass: grradients
+    -Update weights 
+'''
